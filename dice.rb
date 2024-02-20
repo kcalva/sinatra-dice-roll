119e37743ef336a926c26efcc0f4e4dde938970e
Kevin Calva <62715460+kcalva@users.noreply.github.com>
2024-02-20 05:40:13 +0000

    added folder called views which allows to add html embedded in ruby. and a layout file to keep whatever was similar in each route in that layout

--- dice.rb
+++ dice.rb
@@ -10,21 +10,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Dice Roll</h1>  
-  <ul>
-    <li>
-      <a href=/dice/2/6>Roll two 6-sided dice</a> 
-    </li>
-    <li>
-      <a href=/dice/2/10>Roll two 10-sided dice</a>
-    </li>
-    <li>
-      <a href=/dice/1/20>Roll one 20-sided dice</a>
-    </li>
-    <li>
-      <a href=/dice/5/4>Roll five 4-sided dice</a>
-    </li>
-  </ul>"
+ erb(:elephant)
 end
 
 get("/zebra") do
@@ -35,49 +21,47 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+get("/dice/2/6") {
+  @rolls = []
+  2.times{
+    die = rand(1..6)
+    @rolls.push(die)
+  }
+  erb(:two_six)
+}
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+get("/dice/2/10"){
+  @rolls = []
+  2.times{
+    die = rand(1..10)
+    @rolls.push(die)
+  }
+  erb(:two_ten)
+}
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
-end
+get("/dice/1/20"){
+  @rolls = []
+  1.times{
+    die = rand(1..20)
+    @rolls.push(die)
+  }
+  erb(:one_twenty)
+}
 
-get("/dice/2/10") do 
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+get("/dice/5/4"){
+  @rolls = []
+  5.times{
+    die = rand(1..4)
+    @rolls.push(die)
+  }
+  erb(:five_four)
+}
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-  "
-  <h1>2d10</h1>
-  <p>#{outcome}</p>
-  "
-end
-
-get("/dice/1/20") do
-  die = rand(1..20)
-  outcome = "You rolled #{die}"
-  "
-    <h1>1d20</h1>
-    <p>#{outcome}</p>
-  "
-end
-
-get("/dice/5/4") do 
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-  outcome = "You rolled a #{first_die}, a #{second_die} , a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
-  "
-  <h1>5d4</h1>
-  <p>#{outcome}</p>
-  " 
-end
+get("/dice/100/6") {
+  @rolls = []
+  100.times{
+    die = rand(1..6)
+    @rolls.push(die)
+  }
+  erb(:one_hundred_six)
+}
